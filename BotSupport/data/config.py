3b39bb51424f8aa38fdbb26546dc11824f4f5605
anemptyemptiness <103761193+anemptyemptiness@users.noreply.github.com>
2023-05-08 16:51:56 +0300

    completed bot

---
+++ BotSupport/data/config.py
@@ -0,0 +1,5 @@
+TOKEN = 'token'
+
+support_ids = [
+    'support_member_id'
+]
---
+++ BotSupport/handlers/users/__init__.py
@@ -0,0 +1,6 @@
+from .support import dp
+from .support_call import dp
+from .help import dp
+from .start import dp
+
+__all__ = ["dp"]
